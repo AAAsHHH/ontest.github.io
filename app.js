4b73bd4de867a772953fa755751545a23f263fbc
AAAsHHH <101077707+AAAsHHH@users.noreply.github.com>
2024-08-20 00:37:39 +0330

    Update app.js

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ function onSignIn(googleUser) {
     var auth2 = gapi.auth2.getAuthInstance();
     auth2.signOut().then(function () {
       alert("U have been success signooout");
-      $(("#.g-signin2")).css("display","none");
-      $((".data")).css("display","block");
+      $(("#.g-signin2")).css("display","block");
+      $((".data")).css("display","none");
     });
   }
