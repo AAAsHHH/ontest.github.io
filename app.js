06e09b7748789913ed8008340828863521ef85a6
AAAsHHH <101077707+AAAsHHH@users.noreply.github.com>
2024-08-20 00:26:28 +0330

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,21 @@
-function myFunction() {
-  var element = document.body;
-  element.classList.toggle("dark-mode");
-}
+function onSignIn(googleUser) {
+    var profile = googleUser.getBasicProfile();
+    // console.log('ID: ' + profile.getId());
+    // console.log('Name: ' + profile.getName());
+    // console.log('Image URL: ' + profile.getImageUrl());
+    // console.log('Email: ' + profile.getEmail()); 
+    $(("#name")).text(profile.getName());
+    $(("#email")).text(profile.getEmail());
+    $(("#image")).attr('src', profile.getImageUrl());
+    $((".data")).css("display","block");
+    $(("#.g-signin2")).css("display","none");
+    
+  }
+  function signOut() {
+    var auth2 = gapi.auth2.getAuthInstance();
+    auth2.signOut().then(function () {
+      alert("U have been success signooout");
+      $(("#.g-signin2")).css("display","block");
+      $((".data")).css("display","block");
+    });
+  }
