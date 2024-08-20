35498a9e0e2ab4817105e78e3234be9aff6fc57f
AAAsHHH <101077707+AAAsHHH@users.noreply.github.com>
2024-08-20 16:57:15 +0330

    Update index.js

--- index.js
+++ index.js
@@ -1,30 +1,13 @@
   function handleCredentialResponse(response) {
-    const credential = response.credential;
-    const parts = credential.split('.');
-    const payload = JSON.parse(atob(parts[1]));
-  
-    const userId = payload.sub;
-    const name = payload.name;
-    const email = payload.email;
-    const picture = payload.picture;
-  
-    console.log('ID:', userId);
-    console.log('Full Name:', name);
-    console.log('Email:', email);
-    console.log('Profile Picture:', picture);
-    $(("#name")).text(payload.name);
-    $(("#email")).text(payload.email);
-    $(("#image")).attr('src', payload.picture);
-    $((".data")).css("display", "block");
-    $(("#.g-signin2")).css("display", "block");
-  }
- window.onload = function() {
-    google.accounts.id.initialize({
-        client_id: "542701689003-qmngcm1vdsq0vcgfeo0jme0r8iovbnmm.apps.googleusercontent.com",
-        callback: handleCredentialResponse
-    });
+     // decodeJwtResponse() is a custom function defined by you
+     // to decode the credential response.
+     const responsePayload = decodeJwtResponse(response.credential);
 
-    google.accounts.id.renderButton(
-        document.getElementsByClassName("g_id_signin")[0],
-        { theme: "outline", size: "large" }
-    );
+     console.log("ID: " + responsePayload.sub);
+     console.log('Full Name: ' + responsePayload.name);
+     console.log('Given Name: ' + responsePayload.given_name);
+     console.log('Family Name: ' + responsePayload.family_name);
+     console.log("Image URL: " + responsePayload.picture);
+     console.log("Email: " + responsePayload.email);
+  }
+
