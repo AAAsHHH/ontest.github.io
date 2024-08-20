3016ca875f160f1dd072e58a37756ea7f74aa664
AAAsHHH <101077707+AAAsHHH@users.noreply.github.com>
2024-08-20 17:06:46 +0330

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
-window.handleCredentialResponse = (response) => {
-  // decodeJwtResponse() is a custom function defined by you
-  // to decode the credential response.
-  responsePayload = decodeJwtResponse(response.credential);
+function parseJwt (token) {
+   var base64Url = token.split('.')[1];
+   var base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
+  var jsonPayload = decodeURIComponent(atob(base64).split('').map(function(c) {
+    return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
+  }).join(''));
 
-  console.log("ID: " + responsePayload.sub);
-  console.log('Full Name: ' + responsePayload.name);
-  console.log('Given Name: ' + responsePayload.given_name);
-  console.log('Family Name: ' + responsePayload.family_name);
-  console.log("Image URL: " + responsePayload.picture);
-  console.log("Email: " + responsePayload.email);
-}
+  return JSON.parse(jsonPayload);
+};
+ function handleCredentialResponse(response) {
+       //console.log(response)
+      
+        console.log(JSON.stringify(parseJwt(response.credential)));
+    }
